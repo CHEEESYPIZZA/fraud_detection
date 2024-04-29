7ff259f8b92bb6aec8f3b5de177bdbbc922a93d2
CHEEESYPIZZA <dhiraj.salwadi2000@gmail.com>
2024-04-29 14:27:56 +0530

    added 3 new model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("adding all ml related code here")
+
+
+def model1():
+    print('adding code ML model 1 ')
+
+def model2():
+    print('adding code ML model 2 ')
+
+def model3():
+    print('adding code ML model 3 ')
